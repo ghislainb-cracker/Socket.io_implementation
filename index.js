b1657ee9e9faf8af41f99a5871bc03d9fa91ed2a
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-29 23:15:05 +0200

    eigth commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", (client) => {
     console.log("user connected to server ✅");
-    client.emit('message', "welcome to the server")
+    client.emit('message', "")
     client.on("disconnect", () => {
         console.log("user disconnected from the server ❌")
     })
