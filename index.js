e821dca13662c635e17d992486282d53a0706fe7
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-29 23:57:29 +0200

    18 commit

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-import express from "express";
-import http from "http";;
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
-import { Server } from "socket.io"
+// import express from "express";
+// import http from "http";;
+// import { fileURLToPath } from "node:url";
+// import { dirname, join } from "node:path";
+// import { Server } from "socket.io"
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const PORT = 3000
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+// const PORT = 3000
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "index.html"))
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(join(__dirname, "index.html"))
+// })
 
-io.on("connection", (client) => {
-    console.log("user connected to server ✅");
-    client.emit('message', "Server is sending data to the client")
-    client.on("new message", (message) => {
-        console.log(message);
-    })
-    client.on("disconnect", () => {
-        console.log("user disconnected from the server ❌")
-    })
-})
+// io.on("connection", (client) => {
+//     console.log("user connected to server ✅");
+//     client.emit('message', "Server is sending data to the client")
+//     client.on("new message", (message) => {
+//         console.log(message);
+//     })
+//     client.on("disconnect", () => {
+//         console.log("user disconnected from the server ❌")
+//     })
+// })
 
 
-server.listen(PORT, () => {
-    console.log(`server is running on port: http://localhost:${PORT}`)
-})
+// server.listen(PORT, () => {
+//     console.log(`server is running on port: http://localhost:${PORT}`)
+// })
