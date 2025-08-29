874bb681cac9e9b2f7f39a86e10721249865bbf8
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-30 00:48:29 +0200

    22 commit

--- index.js
+++ index.js
@@ -30,3 +30,13 @@
 // server.listen(PORT, () => {
 //     console.log(`server is running on port: http://localhost:${PORT}`)
 // })
+
+import express from "express";
+import http from "node:http";
+import {Server} from "socket.io"
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"))
