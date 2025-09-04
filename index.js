5faa2ae991cba3bcaf7fd9e7a1e63953a0d8efb1
Ghislain Byimbo <byimbog250@gmail.com>
2025-09-04 15:30:40 +0200

    31 commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // app.get("/", (req, res) => {
-//     res.sendFile(join(__dirname, "index.html"))
+//     res.sendFile(join(__dirname, "index.html")) 
 // })
 
 // io.on("connection", (client) => {
